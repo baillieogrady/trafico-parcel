8766eb779c88fa94b46fd0e0fe0b99183ddc72dd
Baillie O'Grady <baillieogrady@Baillies-iMac.local>
2020-05-21 18:00:03 +0100

    Start slider section

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import './scss/index.scss';
 
+import $ from "jquery";
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.scss';
+
 // globals
 const global = {
     body: document.querySelector("body")
 }
 
 // header
-const header = (function(){
+const header = (function () {
     const content = {
         burger: document.querySelector(".burger"),
         headerNav: document.querySelector(".header__nav")
     }
-        
-    const burgerToggle = function() {
+
+    const burgerToggle = function () {
         // toggle to mobile menu when burger icon is clicked
-        content.burger.addEventListener("click", function() {
+        content.burger.addEventListener("click", function () {
             this.classList.toggle("active");
             content.headerNav.classList.toggle("active");
             global.body.classList.toggle("overflow-hidden");
@@ -22,10 +26,34 @@ const header = (function(){
     }
 
     return {
-        callBurgerToggle: function() {
+        callBurgerToggle: function () {
             burgerToggle();
         }
     }
 })();
 
+const slider = (function () {
+    const content = {
+        $slider: $('.slider')
+    }
+
+    const sliderActivate = function () {
+        // toggle to mobile menu when burger icon is clicked
+        content.$slider.slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '300px',
+            // rtl: false
+        });
+    }
+
+    return {
+        callSlickActivate: function () {
+            sliderActivate();
+        }
+    }
+})();
+
 header.callBurgerToggle();
+slider.callSlickActivate();
